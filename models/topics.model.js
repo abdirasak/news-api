b240e4cb1cf26a7f0a6a3f1b97e2dcdef640fb8d
abdirasak <abdi_2020@hotmail.co.uk>
2022-03-31 11:01:22 +0100

    modified the topics.model file

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection");
 
 exports.selectTopics = async (req, res) => {
-  const result = await db.query("SELECT * FROM topics");
+  const result = await db.query("SELECT * FROM topics;");
   return result.rows;
 };
