e4abedc40a47d603473a0949fe55264b26f28b84
abdirasak <abdi_2020@hotmail.co.uk>
2022-03-31 14:30:29 +0100

    fourth commit- modified the articles.controller file - created the GET /api/articles endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -4,6 +4,5 @@ exports.selectArticles = async (req, res) => {
   const result = await db.query(
     "SELECT * FROM articles ORDER BY created_at DESC;"
   );
-  console.log(result.rows, "<< Model");
   return result.rows;
 };
