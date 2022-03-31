7f1c55e167a18069ddcdc4e62ae0dee8b3bb01dd
abdirasak <abdi_2020@hotmail.co.uk>
2022-03-31 14:23:49 +0100

    second commit - created the GET /api/articles endpoint

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,6 +1,9 @@
 const db = require("../db/connection");
 
 exports.selectArticles = async (req, res) => {
-  const result = await db.query("SELECT * FROM articles;");
+  const result = await db.query(
+    "SELECT * FROM articles ORDER BY created_at DESC;"
+  );
+  console.log(result.rows, "<< Model");
   return result.rows;
 };
